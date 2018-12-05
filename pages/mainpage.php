f472f44faee3d22ffe0c5150a8a6596e99af99f3
Tiago Ribeiro <tiago7b27@gmail.com>
2018-12-05 17:12:54 +0000

    mainpage check user fixed

--- pages/mainpage.php
+++ pages/mainpage.php
@@ -5,6 +5,10 @@
   include_once('../templates/tpl_common.php');
   include_once('../templates/tpl_mainpage.php');
 
+  // Verify if user is logged in
+    if (!isset($_SESSION['username']))
+    die(header('Location: ../pages/login.php'));
+
   draw_header($_SESSION['username']);
 
   $storyInfo = listStory('');
