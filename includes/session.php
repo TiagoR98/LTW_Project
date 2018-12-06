1a48161cf5f1032a1b3565ee9de343705dd5c4e2
Tiago Ribeiro <tiago7b27@gmail.com>
2018-12-06 15:35:58 +0000

    csrf protection new comment

--- includes/session.php
+++ includes/session.php
@@ -1,4 +1,11 @@
 <?php
 session_start();
 session_regenerate_id(true);
+if (!isset($_SESSION['csrf'])) {
+  $_SESSION['csrf'] = generate_random_token();
+}
+
+function generate_random_token() {
+  return bin2hex(openssl_random_pseudo_bytes(32));
+}
 ?>
