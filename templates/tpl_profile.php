ba32bfbb8cf75e152e26ef48ffac894d20f4647d
Tiago Ribeiro <tiago7b27@gmail.com>
2018-12-19 16:13:48 +0000

    profile page CSS updated

--- templates/tpl_profile.php
+++ templates/tpl_profile.php
@@ -9,16 +9,16 @@
     <img id="profile_pic" src="../files/croppedProfile/<?php if($userInfo['profilePic']!="") { echo($userInfo['profilePic']); } else {?>default.png<?php } ?>" alt="<?php echo($userInfo['username']); ?>'s profile picture">
 
     <div id="profile_info">
-      <h2>Username: </h2>
+      <h2><i class="fas fa-user"></i>  Username: </h2>
       <h3><?php echo($userInfo['username']); ?></h3><br>
 
-      <h2>Email: </h2>
+      <h2><i class="fas fa-at"></i>  Email: </h2>
       <h3 id="emailInfo"><a href="mailto:<?php echo($userInfo['email']); ?>"><?php echo($userInfo['email']); ?></a>
       <?php if($userInfo['username'] == $_SESSION['username']) { ?>
         <button text='Edit' onclick="edit('email')">Edit</button>
       <?php } ?></h3><br>
 
-      <h2>Birth date: </h2>
+      <h2><i class="fas fa-calendar-alt"></i>  Birth date: </h2>
       <h3 id="birthInfo"><time datetime="<?php echo($userInfo['birth']); ?>"><?php echo($userInfo['birth']); ?></time>
         <?php if($userInfo['username'] == $_SESSION['username']) { ?>
           <button text='Edit' onclick="edit('birth')">Edit</button>
@@ -26,7 +26,7 @@
 
       <?php if($userInfo['username'] == $_SESSION['username']) { ?>
         <div id="uploadPic">
-          <h2>Upload Profile Picture: </h2>
+          <h2><i class="fas fa-image"></i>  Upload Profile Picture: </h2>
           <h3>
               <input type="file" name="profilePic" accept="image/*">
               <input type="submit">
@@ -49,7 +49,7 @@
       <?php if($username == $_SESSION['username']) { ?>
         <ul id="myAddStory">
           <li><h1>My Stories</h1></li>
-          <li><p id="addStory"><a href = "../pages/new_story.php" >Add a story</a></p></li>
+          <li><p id="addStory"><a href = "../pages/new_story.php" ><i class="fas fa-plus"></i> Add a story</a></p></li>
         </ul>
       <?php } else { ?>
         <h1><?php echo $username; ?>'s Stories</h1>
